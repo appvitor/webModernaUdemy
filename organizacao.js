b0eb358212b3182246c9d9b49f16bd79a5a19a37
Vitor P Correa <vitorcorrea11@hotmail.com>
2018-08-29 18:17:02 -0300

    Adicionado como fazer comentário e manipular variáveis

--- organizacao.js
+++ organizacao.js
@@ -1,1 +1,10 @@
-console.log("Olá JavaScript! A quanto tempo!")
+console.log('Olá JavaScript! A quanto tempo!')
+//Comentário de uma linha apenas use barras duplas
+/*
+Comentário de várias linhas, coloque barra e asterisco
+*/
+var num1 = 3
+var num2 = 2
+console.log(num1 + '+' + num2 + '=' + (num1+num2))
+//LET é uma variável local, VAR é sempre global
+//Procure utilizar CONST sempre que possível
